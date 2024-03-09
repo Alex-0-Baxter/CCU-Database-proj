845a7b62e0d977593fc2bce3ca50b5e64c4dfa17
Alex Baxter <156488270+Alex-0-Baxter@users.noreply.github.com>
2024-03-09 13:27:53 -0700

    Add files via upload

---
+++ Categories.sql
@@ -0,0 +1,19 @@
+use master
+GO
+
+DROP DATABASE  IF Exists The_Better_Team_DB 
+GO
+
+CREATE DATABASE  The_Better_Team_DB
+GO
+
+CREATE TABLE Categories(
+	 Cat_ID tinyint NOT NULL
+	,Burgers varchar(26) NULL
+	,Dogs varchar(26) NULL
+	,Sandwiches varchar(26) NULL
+	,Fries varchar(26) NULL
+	,Drinks varchar(26) NULL
+	,Milkshakes varchar(26) NULL
+	,FK_Order_ID varchar(26) NULL
+);
