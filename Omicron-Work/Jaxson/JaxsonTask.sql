b973a6ba612052511662b3a4f7e86642048cc574
0578327 <156855183+0578327@users.noreply.github.com>
2024-04-20 15:58:09 -0600

    Sql now and added in most of my code
    Need to adjust some of the variables

---
+++ Omicron-Work/Jaxson/JaxsonTask.sql
@@ -0,0 +1,176 @@
+-- Drop existing procedures if they exist
+Drop Procedure if Exists BurgerMenuItems
+Go
+
+Drop Procedure if Exists CompleteBurgerMenu
+Go
+
+Drop Procedure if Exists CompleteBurgerToppingsMenu
+Go
+
+Drop Procedure if Exists ToppingsMenuItems
+Go
+
+Drop Procedure if Exists DogsMenuItems
+Go
+
+Drop Procedure if Exists CompleteDogsMenu
+Go
+
+Drop Procedure if Exists CompleteDogsToppingsMenu
+Go
+
+Drop Procedure if Exists CompleteShakesMenu
+Go
+
+Drop Procedure if Exists FriesMenuItems
+Go
+
+Drop Procedure if Exists CompleteFriesMenu
+Go
+
+Drop Procedure if Exists SandwichesMenuItems
+Go
+
+Drop Procedure if Exists CompleteSandwichesMenu
+Go
+
+Drop Procedure if Exists DrinksMenuItems
+Go
+
+Drop Procedure if Exists CompleteDrinksMenu
+Go
+
+Drop Procedure if Exists MixinsMenuItems
+Go
+
+Drop Procedure if Exists CompleteMixinsMenu
+Go
+
+-- BurgerMenuItems Procedure
+Create Procedure BurgerMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Burgers Join Categories On Categories.ID = fk_Category
+	Where Category = 'Burgers' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteBurgerMenu Procedure
+Create Procedure CompleteBurgerMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Burgers Join Categories On Categories.ID = fk_Category
+	Where Category = 'Burgers' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- CompleteBurgerToppingsMenu Procedure
+Create Procedure CompleteBurgerToppingsMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Toppings Join Categories On Categories.ID = fk_Category
+	Where Category = 'Toppings' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- ToppingsMenuItems Procedure
+Create Procedure ToppingsMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Toppings
+	Where Category = 'Toppings' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- DogsMenuItems Procedure
+Create Procedure DogsMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.HotDogs
+	Where Category = 'Hot Dogs' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteDogsMenu Procedure
+Create Procedure CompleteDogsMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.HotDogs Join Categories On Categories.ID = fk_Category
+	Where Category = 'Hot Dogs' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- CompleteDogsToppingsMenu Procedure
+Create Procedure CompleteDogsToppingsMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Toppings Join Categories On Categories.ID = fk_Category
+	Where Category = 'Toppings' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteShakesMenu Procedure
+Create Procedure CompleteShakesMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Shakes Join Categories On Categories.ID = fk_Category
+	Where Category = 'Shakes' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- FriesMenuItems Procedure
+Create Procedure FriesMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Fries
+	Where Category = 'Fries' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteFriesMenu Procedure
+Create Procedure CompleteFriesMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Fries Join Categories On Categories.ID = fk_Category
+	Where Category = 'Fries' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- SandwichesMenuItems Procedure
+Create Procedure SandwichesMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Sandwiches
+	Where Category = 'Sandwiches' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteSandwichesMenu Procedure
+Create Procedure CompleteSandwichesMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Sandwiches Join Categories On Categories.ID = fk_Category
+	Where Category = 'Sandwiches' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- DrinksMenuItems Procedure
+Create Procedure DrinksMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Drinks
+	Where Category = 'Drinks' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteDrinksMenu Procedure
+Create Procedure CompleteDrinksMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Drinks Join Categories On Categories.ID = fk_Category
+	Where Category = 'Drinks' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
+
+-- MixinsMenuItems Procedure
+Create Procedure MixinsMenuItems As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Mixins
+	Where Category = 'Mixins' And MenuPlacement > 0
+	Order By MenuPlacement
+Go
+
+-- CompleteMixinsMenu Procedure
+Create Procedure CompleteMixinsMenu As
+	Select Category, ItemName, Price, Cals, Carbs, TotalFat, Sodium, [Description]
+	From dbo.Mixins Join Categories On Categories.ID = fk_Category
+	Where Category = 'Mixins' And OrderingArrangement > 0
+	Order By OrderingArrangement
+Go
